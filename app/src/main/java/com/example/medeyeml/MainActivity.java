7502aab6e4b9a1c94d128cbc117fef45c9dc99f4
Andre Villanueva <agv.andre@gmail.com>
2022-09-17 16:50:52 -0400

    Implemented tflite model into MainActivity.

--- app/src/main/java/com/example/medeyeml/MainActivity.java
+++ app/src/main/java/com/example/medeyeml/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.medeyeml;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.nfc.Tag;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -17,6 +23,15 @@ import com.example.medeyeml.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.task.gms.vision.detector.Detection;
+import org.tensorflow.lite.task.gms.vision.detector.ObjectDetector;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -73,4 +88,57 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+    //TensorFlow Object Detection
+    private void runObjectDetection(Bitmap bitmap) throws IOException {
+        //Convert reg pics to tensorimages
+        TensorImage image = TensorImage.fromBitmap(bitmap);
+
+        //Initialize a detector object
+        ObjectDetector.ObjectDetectorOptions options = ObjectDetector.ObjectDetectorOptions.builder().setMaxResults(5)
+                .setScoreThreshold(0.5f)
+                .build();
+        ObjectDetector objectDetector = ObjectDetector.createFromFileAndOptions(this,"skinDetectorModel.tflite",options);
+
+        //Perform detection
+        List<Detection> results = objectDetector.detect(image);
+
+        //debugPrint(results);
+
+        //Draw & Get detection results
+        List<String> diseases = new ArrayList<>();
+        Canvas drawnResultsImg = new Canvas(bitmap);
+        Paint paint = new Paint(Paint.FILTER_BITMAP_FLAG);
+        for(Detection detection : results) {
+            String curDiseaseFound = detection.getCategories().get(0).getLabel();
+            diseases.add(curDiseaseFound);
+            drawnResultsImg.drawRect(detection.getBoundingBox(), paint);
+        }
+
+        //TODO Figure out how to do change inputImageView
+        //https://www.youtube.com/watch?v=vLxn5mOuWAk (31:37)
+        MainActivity.this.runOnUiThread(new Runnable(){
+            @Override
+            public void run() {
+
+            }
+        });
+
+
+    }
+
+    //Print Results
+    private void debugPrint(List<Detection> results){
+        int i = 0;
+        for(Detection detection : results){
+            RectF box = detection.getBoundingBox();
+
+            Log.d("Index of Object", Integer.toString(i));
+            Log.d("Class of Object", detection.getCategories().get(0).getLabel());
+            Log.d("Bounding Box", Float.toString(box.centerX()));
+
+            i++;
+        }
+    }
+
 }
